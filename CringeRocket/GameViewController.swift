0e069cc08b2fd34da7dda4fd3a464f3a56d18d85
m1miage-fabryd <dorianfabry@gmail.com>
2022-06-20 18:21:11 +0200

    boat + music/sounds

--- CringeRocket/GameViewController.swift
+++ CringeRocket/GameViewController.swift
@@ -12,26 +12,27 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var rocket: UIImageView!
     @IBOutlet weak var rocketLeftMargin: NSLayoutConstraint!
-    
     @IBOutlet weak var ship: UIImageView!
-    
-    // TODO : murs
-    
+        
     var horizontalSpeed = 0.0
     var verticalSpeed = 0.0
-    var playerPosition = 3
+    var playerPosition = 2
+    var playerVerticalPosition = 6
     var horizontalColumns = 5
-    var verticalColumns = 5
+    var verticalColumns = 10
     var stateShip = 1
     
     var obstacles: Array<UIImageView> = Array()
+    var aliveObstacles: Array<Int> = Array()
     var obstaclesPosition: Array<Int> = Array()
+    
+    var timer: Timer!
+    
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // TODO: AVAudioPlayer(contentsOf: <#T##URL#>)
-        
+                
         horizontalSpeed = view.frame.size.width / Double(horizontalColumns)
         verticalSpeed = view.frame.size.height / Double(verticalColumns)
         rocketLeftMargin.constant = (view.frame.size.width - rocket.frame.width) / 2
@@ -42,66 +43,100 @@ class GameViewController: UIViewController {
             obstacle.image =  UIImage(named: "the_colossal")
             view.addSubview(obstacle)
             obstacles.append(obstacle)
+            aliveObstacles.append(i)
             obstaclesPosition.append(0)
         }
         
-        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        playAudioAsset("music")
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
     }
     
     @objc func fireTimer() {
-        print("Timer fired!")
-        let randomInt = Int.random(in: 0...obstacles.count - 1)
-        obstacles[randomInt].frame.origin.y += verticalSpeed
-        obstaclesPosition[randomInt] += 1
+        let random = aliveObstacles.randomElement()!
+        obstacles[random].frame.origin.y += verticalSpeed
+        obstaclesPosition[random] += 1
         
-        if obstaclesPosition[randomInt] > verticalColumns {
-            obstacles[randomInt].removeFromSuperview()
-            obstacles.remove(at: randomInt)
+        if obstaclesPosition[random] > verticalColumns {
+            removeObstacle()
         }
         
-        if obstaclesPosition[randomInt] == 4 && randomInt == playerPosition {
-            obstacles[randomInt].removeFromSuperview()
-            obstacles.remove(at: randomInt)
-            
+        if obstaclesPosition[random] == playerVerticalPosition && random == playerPosition {
+            removeObstacle()
         }
         
-        if obstaclesPosition[randomInt] == 5  {
-            obstacles[randomInt].removeFromSuperview()
-            obstacles.remove(at: randomInt)
+        if obstaclesPosition[random] == 9 {
+            removeObstacle()
             stateShip += 1
             ship.image = UIImage(named: "thousand-sunny-" + String(stateShip))
-            if stateShip == 0 {
-                gameOver()
+            if stateShip >= 4 {
+                gameOver(false)
             }
         }
         
-        if obstacles.count == 0 {
-            gameOver()
+        print(aliveObstacles.count)
+        
+        if aliveObstacles.count == 0 {
+            gameOver(true)
         }
+        
+        print(obstaclesPosition)
+                
+        func removeObstacle() {
+            playAudioAsset("sword")
+            
+            obstacles[random].removeFromSuperview()
+            aliveObstacles = aliveObstacles.filter {$0 != random}
+        }
+        
     }
     
     func goRight() {
-        if playerPosition < horizontalColumns {
+        if playerPosition < horizontalColumns - 1 {
             rocketLeftMargin.constant += horizontalSpeed
             playerPosition += 1
         }
         
-        print(rocketLeftMargin.constant)
+        checkObstacle()
+        
         print(playerPosition)
     }
     
     func goLeft() {
-        if playerPosition > 1 {
+        if playerPosition > 0 {
             rocketLeftMargin.constant -= horizontalSpeed
             playerPosition -= 1
         }
         
-        print(rocketLeftMargin.constant)
+        checkObstacle()
+                
         print(playerPosition)
     }
     
-    func gameOver() {
+    func checkObstacle() {
+        if obstaclesPosition[playerPosition] == playerVerticalPosition {
+            playAudioAsset("sword")
+
+            obstacles[playerPosition].removeFromSuperview()
+            aliveObstacles = aliveObstacles.filter {$0 != playerPosition}
+            obstaclesPosition[playerPosition] = -1
+        }
+    }
+    
+    func gameOver(_ win: Bool) {
+        timer.invalidate()
+        
         print("partie terminée")
+        
+        let alert = UIAlertController(title: "Partie terminée", message: win ? "GG" : "claqué au sol", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            exit(EXIT_SUCCESS)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -111,6 +146,19 @@ class GameViewController: UIViewController {
             goLeft()
         } else {
             goRight()
+        }
+    }
+    
+    func playAudioAsset(_ assetName : String) {
+          guard let audioData = NSDataAsset(name: assetName)?.data else {
+             fatalError("Unable to find asset \(assetName)")
+          }
+
+          do {
+             audioPlayer = try AVAudioPlayer(data: audioData)
+             audioPlayer.play()
+          } catch {
+             fatalError(error.localizedDescription)
         }
     }
 
